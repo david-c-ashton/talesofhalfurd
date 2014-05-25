d1ffab5f6d0cd07c6972cac4ca63c97b361ee55d
David Ashton <david_c_ashton@live.com>
2014-05-24 21:42:37 -0400

    Basic spell methods

--- C#/Spell.cs
+++ C#/Spell.cs
@@ -4,7 +4,8 @@ using System.Collections;
 public struct mSpell
 {
 	public int power;
-	public int cost;
+	public int mana;
+	public float duration;
 	public string name;
 	public mDOMAIN domain;
 	public aTYPE type;
@@ -13,4 +14,74 @@ public struct mSpell
 public class Spell
 {
 	public mSpell spell;
+
+	public void setSpellPower(int p)
+	{
+		this.spell.power = p;
+	}
+
+	public int getSpellPower()
+	{
+		return this.spell.power;
+	}
+
+	public void setSpellMana(int m)
+	{
+		this.spell.mana = m;
+	}
+
+	public int getSpellMana()
+	{
+		return this.spell.mana;
+	}
+
+	public void setSpellDuration(float dur)
+	{
+		this.spell.duration = dur;
+	}
+
+	public float getSpellDuration()
+	{
+		return this.spell.duration;
+	}
+
+	public void setSpellName(string n)
+	{
+		this.spell.name = n;
+	}
+
+	public string getSpellName()
+	{
+		return this.spell.name;
+	}
+
+	public void setSpellDomain(mDOMAIN dom)
+	{
+		this.spell.domain = dom;
+	}
+
+	public mDOMAIN getSpellDomain()
+	{
+		return this.spell.domain;
+	}
+
+	public void setSpellType(aTYPE t)
+	{
+		this.spell.type = t;
+	}
+
+	public aTYPE getSpellType()
+	{
+		return this.spell.type;
+	}
+
+	public void debug()
+	{
+		Debug.Log (this.spell.power.ToString ());
+		Debug.Log (this.spell.mana.ToString ());
+		Debug.Log (this.spell.duration.ToString ());
+		Debug.Log (this.spell.name);
+		Debug.Log (this.spell.domain.ToString());
+		Debug.Log (this.spell.type.ToString ());
+	}
 }
