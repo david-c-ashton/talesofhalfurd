b69b4741939fff227795639c70b28b028fb38a58
David Ashton <david_c_ashton@live.com>
2014-05-27 19:40:04 -0400

    Created test for generating player

--- C#/MonoBehaviors/Test.cs
+++ C#/MonoBehaviors/Test.cs
@@ -2,26 +2,12 @@
 using System.Collections;
 
 public class Test : MonoBehaviour {
-	public Spell spell1 = new Spell();
-	public Spell spell2 = new Spell();
+	public Player mPlayer = new Player();
 
 	// Use this for initialization
 	void Start () {
-		this.spell1.setSpellPower (7);
-		this.spell1.setSpellMana (2);
-		this.spell1.setSpellDuration (0);
-		this.spell1.setSpellName ("Fire Bolt");
-		this.spell1.setSpellDomain (mDOMAIN.FIRE);
-		this.spell1.setSpellType (aTYPE.SINGLE);
-
-		this.spell2.setSpellPower (3);
-		this.spell2.setSpellMana (1);
-		this.spell2.setSpellDuration (2.4f);
-		this.spell2.setSpellName ("Light Cure");
-		this.spell2.setSpellDomain (mDOMAIN.BODY);
-		this.spell2.setSpellType (aTYPE.SUPPORT);
-
-		this.spell1.debug ();
-		this.spell2.debug ();
+		CSV.items = CSV.loadItems ();
+		this.mPlayer.setNewCharacter ("Holfurd");
+		Debug.Log (this.mPlayer.player.inventory.weapon.itemName);
 	}
 }
