8ee3cee55da8438c7c3d94cb85e14f8eba1946e0
David Ashton <david_c_ashton@live.com>
2014-05-29 21:00:02 -0400

    NPC Initialization

---
+++ C#/NPC.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public struct pNPC 
+{
+	public int pId;
+	public int action;
+	public GameObject obj;
+}
+
+public class NPC
+{
+	public pNPC npc;
+
+	public void setNpcId(int id)
+	{
+		this.npc.pId = id;
+	}
+
+	public int getNpcId()
+	{
+		return this.npc.pId;
+	}
+
+	public void setNpcAction(int a)
+	{
+		this.npc.action = a;
+	}
+
+	public int getNpcAction()
+	{
+		return this.npc.action;
+	}
+
+	public void setNpcObj(GameObject o)
+	{
+		this.npc.obj = o;
+	}
+
+	public GameObject getNpcObj()
+	{
+		return this.npc.obj;
+	}
+}
